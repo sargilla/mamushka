95297f04cb0795ea7cfd3db3a30f7466333976b9
santiago argilla <sargilla@gmail.com>
2016-02-24 08:05:49 -0300

    testings

---
+++ tests/RussianCachingTest.php
@@ -0,0 +1,23 @@
+<?php 
+
+use Sargilla\Dolly\RussianCaching;
+
+class RussianCachingTest extends TestCase
+{
+		/** @test */
+		function it_caches_a_given_key()
+		{
+			$post = $this->makePost();
+
+			$cache = new \Illuminate\Cache\Repository(
+				new \Illuminate\Cache\ArrayStore
+			);
+
+			$doll = new RussianCaching($cache);
+
+			$doll->cache($post, '<div>view fragment</div>');
+			
+			$this->assertTrue($doll->hasCached($post->getCacheKey()));
+			$this->assertTrue($doll->hasCached($post));
+		}
+}
