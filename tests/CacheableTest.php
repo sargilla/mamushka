5cfad3d6309b0defc4566404944c7cf15a2918ec
Santiago Argilla <u176965@telecom.arg.telecom.com.ar>
2016-02-24 09:13:40 -0300

    testing2

--- tests/CacheableTest.php
+++ tests/CacheableTest.php
@@ -6,6 +6,7 @@ class CacheableTest extends TestCase
 		function it_gets_a_unique_cache_key_for_an_eloquent_model()
 		{
 			$model = $this->makePost();
+			
 			$this->assertEquals(
 				'Post/1-'. $model->updated_at->timestamp,
 				$model->getCacheKey()
